f2c44aa532806287168138e921657b5d1f7d9ec2
Thiện Đức <ductdt@missingcorner.com>
2022-11-06 23:51:47 +0700

    feat: env

--- libs/core/src/config/configuration.ts
+++ libs/core/src/config/configuration.ts
@@ -1,4 +1,4 @@
 export const configuration = () => ({
-  environment: process.env.NODE_ENV,
+  environment: process.env.NODE_ENV || 'development',
   port: parseInt(process.env.PORT || '3000', 10),
 })
